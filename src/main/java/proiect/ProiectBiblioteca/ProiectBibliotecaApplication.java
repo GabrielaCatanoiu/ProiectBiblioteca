1b6a9ce5357066ce4fdfee350d0d942df832190a
GabrielaCatanoiu <118853817+GabrielaCatanoiu@users.noreply.github.com>
2022-12-23 15:16:58 +0200

    Initial commit

---
+++ src/main/java/proiect/ProiectBiblioteca/ProiectBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package proiect.ProiectBiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proiect/ProiectBiblioteca/ProiectBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package proiect.ProiectBiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
